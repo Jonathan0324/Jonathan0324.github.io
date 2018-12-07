8a8729aaeb724cf658b77aa299d5cb252b935924
Jonathan0324 <45168500+Jonathan0324@users.noreply.github.com>
2018-12-08 01:52:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+function swap(val) {
+    if(val=="first") {
+        first.style.display='block';
+        second.style.display='none';
+        third.style.display='none';
+        fourth.style.display='none';
+//        a.style.display='block';   
+//        b.style.display='none';
+//        c.style.display='none';
+//        d.style.display='none';
+    }else if(val=="second") {
+        first.style.display='none';
+        second.style.display='block';
+        third.style.display='none';
+        fourth.style.display='none';
+//        a.style.display='none';   
+//        b.style.display='block';
+//        c.style.display='none';
+//        d.style.display='none';
+    }else if(val=="third") {
+        first.style.display='none';
+        second.style.display='none';
+        third.style.display='block';
+        fourth.style.display='none';
+//        a.style.display='none';   
+//        b.style.display='none';
+//        c.style.display='block';
+//        d.style.display='none';
+    }else if(val=="fourth") {
+        first.style.display='none';
+        second.style.display='none';
+        third.style.display='none';
+        fourth.style.display='block';
+//        a.style.display='none';   
+//        b.style.display='none';
+//        c.style.display='none';
+//        d.style.display='block';
+    }
+//    }else if(val=="a") {
+//        a.style.display='block';   
+//        b.style.display='none';
+//        c.style.display='none';
+//        d.style.display='none';
+//    }else if(val=="b") {
+//        a.style.display='none';   
+//        b.style.display='block';
+//        c.style.display='none';
+//        d.style.display='none';
+//    }else if(val=="c") {
+//        a.style.display='none';   
+//        b.style.display='none';
+//        c.style.display='block';
+//        d.style.display='none';
+//    }else if(val=="d") {
+//        a.style.display='none';   
+//        b.style.display='none';
+//        c.style.display='none';
+//        d.style.display='block';
+//    }
+}
